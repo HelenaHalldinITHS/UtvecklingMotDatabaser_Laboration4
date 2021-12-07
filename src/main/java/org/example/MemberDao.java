8033bf3dcc070dfa0c70f9f01c0cbd060d7dcbbe
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-07 13:32:27 +0100

    Adds interface MemberDao with methods

---
+++ src/main/java/org/example/MemberDao.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public interface MemberDao {
+    Member getById(int id);
+    List<Member> getAll();
+    void update(Member student);
+    void create(Member student);
+    void delete(Member student);
+    List<Member> getActiveMembers();
+    List<Member> getNotActiveMembers();
+}
