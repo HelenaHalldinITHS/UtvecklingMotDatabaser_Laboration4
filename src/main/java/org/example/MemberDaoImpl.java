a618f9b965d98511ec9231cd733ae3cf6e0a7d32
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-07 13:33:28 +0100

    Adds class MemberDaoImpl with unfinished implementations

---
+++ src/main/java/org/example/MemberDaoImpl.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.List;
+
+public class MemberDaoImpl implements MemberDao{
+    @Override
+    public Member getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Member> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Member student) {
+
+    }
+
+    @Override
+    public void create(Member student) {
+
+    }
+
+    @Override
+    public void delete(Member student) {
+
+    }
+
+    @Override
+    public List<Member> getActiveMembers() {
+        return null;
+    }
+
+    @Override
+    public List<Member> getNotActiveMembers() {
+        return null;
+    }
+}
