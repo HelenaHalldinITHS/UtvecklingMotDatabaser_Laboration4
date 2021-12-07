ecddd3ab828945cc9018dbf8ff006ee8f52e4cd9
HelenaHalldinITHS <helena.halldin@iths.se>
2021-12-07 13:26:28 +0100

    Makes class Member an entity

--- src/main/java/org/example/Member.java
+++ src/main/java/org/example/Member.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String firstName;
     private String lastName;
     private boolean active;
